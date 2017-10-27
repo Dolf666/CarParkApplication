a61167158bc32bc19cba2627a75323f3d49310a0
Rudolph <rudolphn34@gmail.com>
2017-10-27 13:29:58 +0100

    add

--- CarParkApplication/src/comcarpark/MainController.java
+++ CarParkApplication/src/comcarpark/MainController.java
@@ -18,7 +18,7 @@ public class MainController {
 		list.add(o3);
 		list.add(o4);
 		list.add(o5);
-		//EventQueue.invokeLater(new Runnable() {
+		//EventQueue.invokeLater(new Runnable() {/////tttttt
 			//public void run() {
 				try {
 					MainScreen frame = new MainScreen();
