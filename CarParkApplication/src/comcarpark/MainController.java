b2f29ada0c8d0694fa17850eb722738992403942
Rudolph <rudolphn34@gmail.com>
2017-10-27 13:24:11 +0100

    reg class added

--- CarParkApplication/src/comcarpark/MainController.java
+++ CarParkApplication/src/comcarpark/MainController.java
@@ -8,8 +8,16 @@ public class MainController {
 	public static void main(String[] args) {
 	 list = new ArrayList<User>();
 		
-		Owner o1=new Owner("John","Kenguroo", "7474747474747", "john@gmail.com", "Lane Street","java","java");
+		Owner o1=new Owner("admin","Kenguroo", "7474747474747", "john@gmail.com", "Lane Street","admin","admin");
+		Owner o2=new Owner("John","Kenguroo", "7474747474747", "john@gmail.com", "Lane Street","java","java");
+		Owner o3=new Owner("Sara","Zebra", "7474747474747", "john@gmail.com", "Lane Street","ace","ace");
+		Owner o4=new Owner("David","Kenguroo", "7474747474747", "john@gmail.com", "Lane Street","lio","lio");
+		Owner o5=new Owner("Catherine","Kenguroo", "7474747474747", "john@gmail.com", "Lane Street","spring","spring");
 		list.add(o1);
+		list.add(o2);
+		list.add(o3);
+		list.add(o4);
+		list.add(o5);
 		//EventQueue.invokeLater(new Runnable() {
 			//public void run() {
 				try {
